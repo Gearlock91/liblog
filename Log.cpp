7f0336cc55acfdb9df95d866b47e08ba724b1954
Andreas Roghe <andreas.roghe@gmail.com>
2025-11-01 13:18:00 +0100

    Solve remove color in logfile

--- Log.cpp
+++ Log.cpp
@@ -22,23 +22,21 @@ class ImplLogger {
     [[nodiscard]] bool             getColored() const;
     [[nodiscard]] std::string_view getLogFilePath() const;
     [[nodiscard]] bool             isDebug() const;
-    [[nodiscard]] std::string      getLogLevelColor(LogLevel level) const;
+    static std::string             getLogLevelColor(LogLevel level, bool colored);
     static std::string             logLevelFormat(LogLevel level);
 
    private:
     ImplLogger() = default;
-
-    void run();
-
+    void               run();
     static std::string getCurrentTimeString();
 
-    std::string_view        mLogFilePath;
-    std::queue<std::string> mLogQueue;
-    std::mutex              mLogMutex;
-    std::thread             mThread;
-    bool                    mRunning = true;
-    bool                    mColored = false;
-    bool                    mDebug   = false;
+    std::string_view                       mLogFilePath;
+    std::queue<std::array<std::string, 4>> mLogQueue;
+    std::mutex                             mLogMutex;
+    std::thread                            mThread;
+    bool                                   mRunning = true;
+    bool                                   mColored = false;
+    bool                                   mDebug   = false;
 };
 
 std::string ImplLogger::logLevelFormat(const LogLevel level) {
@@ -55,24 +53,24 @@ std::string ImplLogger::logLevelFormat(const LogLevel level) {
     return "UNKNOWN LOG LEVEL";
 }
 
-std::string ImplLogger::getLogLevelColor(const LogLevel level) const {
+std::string ImplLogger::getLogLevelColor(const LogLevel level, const bool colored) {
     std::string result;
     switch (level) {
         case LogLevel::DEBUG:
-            result = mColored ? std::string{"\033[34m" + logLevelFormat(LogLevel::DEBUG) + "\033[0m"}
-                              : logLevelFormat(LogLevel::DEBUG);
+            result = colored ? std::string{"\033[34m" + logLevelFormat(LogLevel::DEBUG) + "\033[0m"}
+                             : logLevelFormat(LogLevel::DEBUG);
             break;
         case LogLevel::INFO:
-            result = mColored ? std::string{"\033[32m" + logLevelFormat(LogLevel::INFO) + "\033[0m"}
-                              : logLevelFormat(LogLevel::INFO);
+            result = colored ? std::string{"\033[32m" + logLevelFormat(LogLevel::INFO) + "\033[0m"}
+                             : logLevelFormat(LogLevel::INFO);
             break;
         case LogLevel::WARN:
-            result = mColored ? std::string{"\033[33m" + logLevelFormat(LogLevel::WARN) + "\033[0m"}
-                              : logLevelFormat(LogLevel::WARN);
+            result = colored ? std::string{"\033[33m" + logLevelFormat(LogLevel::WARN) + "\033[0m"}
+                             : logLevelFormat(LogLevel::WARN);
             break;
         case LogLevel::ERROR:
-            result = mColored ? std::string{"\033[31m" + logLevelFormat(LogLevel::ERROR) + "\033[0m"}
-                              : logLevelFormat(LogLevel::ERROR);
+            result = colored ? std::string{"\033[31m" + logLevelFormat(LogLevel::ERROR) + "\033[0m"}
+                             : logLevelFormat(LogLevel::ERROR);
             break;
     }
     return result;
@@ -93,8 +91,12 @@ ImplLogger &ImplLogger::instance() {
 void ImplLogger::setLogFilePath(const std::string_view path) { mLogFilePath = path; }
 
 void ImplLogger::addToQueue(const LogLevel level, const std::string &message) {
-    std::lock_guard<std::mutex> lock(mLogMutex);
-    mLogQueue.push("[" + getCurrentTimeString() + "]" + getLogLevelColor(level) + message + "\n");
+    std::lock_guard<std::mutex>      lock(mLogMutex);
+    const std::array<std::string, 4> parts{"[" + getCurrentTimeString() + "]",
+                                           getLogLevelColor(level, mColored),
+                                           getLogLevelColor(level, false),
+                                           message + "\n"};
+    mLogQueue.push(parts);
 }
 
 void ImplLogger::setColored(const bool colored) { mColored = colored; }
@@ -115,8 +117,8 @@ void ImplLogger::run() {
     FILE *file = std::fopen(mLogFilePath.data(), "a");
     while (mRunning || !mLogQueue.empty()) {
         if (!mLogQueue.empty()) {
-            std::print("{}", mLogQueue.front());
-            std::print(file, "{}", mLogQueue.front());
+            std::print("{}{}{}", mLogQueue.front()[0], mLogQueue.front()[1], mLogQueue.front()[3]);
+            std::print(file, "{}{}{}", mLogQueue.front()[0], mLogQueue.front()[2], mLogQueue.front()[3]);
             mLogQueue.pop();
         }
     }
